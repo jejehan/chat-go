9b6ed022fb102db88a110ba26822ffb370516285
jejehan <seventpoint@gmail.com>
2017-06-17 01:43:48 +0700

    save

--- main.go
+++ main.go
@@ -1,3 +1,9 @@
+//golang memiliki keunikan dalam menjalankan programnya
+//jika kita menginginkan menjalankan program secara langsung dengan
+//mengetikan run main.go secara langsung dari command line kita
+//bisa gunakan "package main"
+//dan jika menginginkan package ini menjadi reusable maka harus diganti
+//menjadi "package chat"
 package main
 
 import (
@@ -9,10 +15,26 @@ func main() {
 
 	var port = ":9090"
 
+	//http HandleFunc
+	//jika user mengakses path patten / fungsi dibawah akan teresekusi
+	//sehingga nantinya akan tampil data yang ditulis seperti dibawah ini
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`
+			<html>
+				<head>
+					<title> Go Chat </title>
+				</head>
+				<body>
+					Sedang dalam pengembangan...
+				</body>
+			</html>
+		`))
+	})
+
 	//start webserver
 	log.Println("ListenAndServe: localhost:", port)
 	if err := http.ListenAndServe(port, nil); err != nil {
-		log.Println("ListenAndServe:", err)
+		log.Println("Failed ListenAndServe:", err)
 	}
 
 }
