4807c8ea2516c525f120b6a556202dca0c1a5ce3
jejehan <seventpoint@gmail.com>
2017-06-17 02:33:28 +0700

    save

--- main.go
+++ main.go
@@ -7,29 +7,61 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
 )
+
+// Memisahkan View dari Logic menggunakan Template
+// Seperti website pada umumnya, biasanya kita akan memisahkan
+// Bisnis logic dengan view agar mudah dimaintain dan pekerjaan
+// menjadi lebih terfokuskan.
+// Kali ini kita akan coba membuat template dimana nantinya,
+// View akan menampilkan data nama yang berasal dari layer logic
+// dan menempatkanya pada template sebagai layer persentasi / view
+//
+// disini kita akan menggunakan beberapa package lain
+// golang memiliki standart library html/template dimana package ini
+// dapat membaca tag-tag html yang dapat dibaca oleh browser.
+// package ini juga turunan dari text/template dimana text/template
+// hanya dapat digunakan untuk merender text saja bukan tag html
+//
+// disini kita juga menggunakan package sync.Once, package ini akan
+// memastikan bahwa dia akan tampil dalam satu action
+//
+// tambahan package selanjunta adalah paht/filepath
+// tugas package ini dapat mencari file yang dinginkan untuk diparse
+// oleh http/template
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServerHTTP ini akan menangangi http request
+// ini adalah method turunan dari struct templateHandler
+// ServeHTTP method yang harus ada karena nantinya akan digunakan
+// pada http.Handle, http.Handle sendiri memiliki parameter
+// pattern dengan tipe String dan handle dengan tipe Handler
+// tipe Handler adalah sebuah interface yang membutukan method ServeHTTP
+//
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
 
 func main() {
 
 	var port = ":9090"
 
-	//http HandleFunc
-	//jika user mengakses path patten / fungsi dibawah akan teresekusi
-	//sehingga nantinya akan tampil data yang ditulis seperti dibawah ini
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-			<html>
-				<head>
-					<title> Go Chat </title>
-				</head>
-				<body>
-					Sedang dalam pengembangan...
-				</body>
-			</html>
-		`))
-	})
+	// http Handle
+	// mengganti HandleFunc menjadi Handle karena kebutuhan parameter, hanya menggunakan struct
+	// struct yang harus memiliki method ServeHTTP
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 
 	//start webserver
 	log.Println("ListenAndServe: localhost:", port)
